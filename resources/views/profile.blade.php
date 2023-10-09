acf1cad664fb9d7967a499814079d7bfe0e24ae6
MuhammadRedhoSaputra <102501722+MuhammadRedhoSaputra@users.noreply.github.com>
2023-10-09 13:10:00 +0700

    9-oct-2023

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel</title>
+</head>
+<body>
+    <div>
+        <h1>Nama Saya Peter</h1>
+        <a href="http://127.0.0.1:8000">< button type="button">Mau Balik</button></a>
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,43 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Buat route ke halaman profil
+Route::get("/profil", function(){
+    return view('profile');
+});
+
+Route::get("/mahasiswa{nama}",function($nama = "Peter") {
+    echo "<h1>Halo Nama Saya $nama</h2>";
+});
+
+Route::get("/mahasiswa2{nama?}",function($nama = "Peter") {
+    echo "<h1>Halo Nama Saya $nama</h2>";
+});
+
+Route::get("/mahasiswa{nama?}/{pekerjaan?}",function($nama = "Peter", $pekerjaan = "Mahasiswa") {
+    echo "<h1>Halo Nama Saya $nama. Saya adalah $pekerjaan</h2>";
+});
+
+//Redirect dan Named Routed 
+Route::get("/hubungi", function(){ 
+
+    echo "<h1>Hubungi Kami</h1>";
+})->name("call"); //named route
+
+Route::redirect("/contact", '/hubungi');
+
+Route::get("/halo", function(){
+    echo "<a href='". route('call') . "'>" . route('call'). "</a>";
+});
+
+route::prefix("/mahasiswa")->group(function(){
+    route::get("/jadwal", function(){
+        echo "<h1>Jadwal Dosen</h1>";
+});
+
+    route::get("/materi", function(){
+        echo "<h1>Materi Perkuliahan</h1>";
+    });
+    //dan lain2
+});
