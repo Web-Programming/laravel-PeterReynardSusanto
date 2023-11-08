8e8d7fe70ac421aacaf42dff306083cc3c4f9647
RendyAmarullah <Sajarendi1234@gmail.com>
2023-11-08 13:13:00 +0700

    form

--- app/Http/Controllers/ProdiController.php
+++ app/Http/Controllers/ProdiController.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\prodi;
 
-
 class ProdiController extends Controller
 {
     public function index() {
@@ -20,7 +19,7 @@ class ProdiController extends Controller
 
     }
     public function allJoinElq() {
-        
+
         $prodis = prodi::with('mahasiswas')->get();
         foreach ($prodis as $prodi) {
             echo "<h3>[$prodi->nama]</h3>";
@@ -30,5 +29,27 @@ class ProdiController extends Controller
             }
             echo "<hr>";
         }
+    }
+    public function create() {
+        return view('prodi.create');
+    }
+    public function store(Request $request)
+     {
+        //dump($request);
+       // echo $request->nama;
+
+       $validateData =$request->validate([
+        'nama' => 'required|min:5|max:20',
+       ]);
+      // dump($validateData);
+      // echo $validateData['nama'];
+
+      $prodi = new prodi();
+      $prodi->nama = $validateData['nama'];//simpan nilai input ($validateData['nama]) ke dalam
+      $prodi->save();   //simpan ke dalam tabel prodi
+
+      //return "Data prodi $prodi->nama berhasil disimpan ke database"; // tampilan pesan berhasil
+       $request->session()->flash('info', "Data prodi $prodi->nama berhasil disimpan ke database");
+       return redirect('prodi/create');
     }
 }
